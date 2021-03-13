a5c6fdbe044de189ee3d4795d9e06bbfa10be76a
omniboyok <barrea.pablo@gmail.com>
2021-03-13 16:52:29 -0300

    Server base route for ping a welcome message

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT;
+var WeatherRouter = require("./src/router/WeatherRouter");
 
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.get("/", function (req, res, next) {
+  return res.json({ message: "Welcome"});
+});
+
+app.use(WeatherRouter);
 
 app.listen(port, function () {
   console.log("Server running on port", port);

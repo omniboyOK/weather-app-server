841000d5ac98bedb2a166d9c2048a834aefa936b
omniboyok <barrea.pablo@gmail.com>
2021-03-14 15:32:09 -0300

    better error handling

--- src/service/LocationService.js
+++ src/service/LocationService.js
@@ -1,23 +1,28 @@
 const ipapi = require("ipapi.co");
 const Service = {
   getLocation: async function (req, res, next) {
-    if (req.params.city) {
+    const { city } = req.params;
+    const { ip } = req;
+
+    if (city) {
       res.locals.location = req.params.city;
       return next();
     }
 
-    const { ip } = req;
-    ipapi.location(function (result) {
-      if (result.error) {
-        res
-          .status(404)
-          .json({ city: "Unkown", error: "Could not get location" });
-      } else {
-        res.locals.location = result;
-        return next();
-        // res.status(200).json(result);
-      }
-    }, ip);
+    if (ip) {
+      ipapi.location(function (result) {
+        if (result.error) {
+          return res.status(400).json({ error: "couldn't get location by ip" });
+        } else {
+          res.locals.location = result;
+          return next();
+        }
+      }, ip);
+    } else {
+      return res
+        .status(400)
+        .json({ error: "missing header data and city parameter" });
+    }
   },
 };
 

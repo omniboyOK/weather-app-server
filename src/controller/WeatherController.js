3c8b4091d71b752e92572fd18c2d56bd54e1c51e
omniboyok <barrea.pablo@gmail.com>
2021-03-14 15:32:01 -0300

    weather api call

--- src/controller/WeatherController.js
+++ src/controller/WeatherController.js
@@ -1,6 +1,20 @@
+const WeatherService = require("../service/WeatherService");
+
 const Controller = {
-  getCityWeather: async function (req, res) {
-    
+  getCityWeather: function (req, res) {
+    let { location } = res.locals;
+
+    if (!location) {
+      return res.status(400).json({ message: "bad request" });
+    }
+
+    WeatherService.getWeatherByCity(location)
+      .then((result) => {
+        return res.status(200).json(result);
+      })
+      .catch((error) => {
+        return res.status(404).json({ message: "city not found" });
+      });
   },
 };
 

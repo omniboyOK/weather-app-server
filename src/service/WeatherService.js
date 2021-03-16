94302656b01bf6ea26787f5dfedac2561bca8757
omniboyok <barrea.pablo@gmail.com>
2021-03-16 01:13:00 -0300

    added metric system to weather api endpoints

--- src/service/WeatherService.js
+++ src/service/WeatherService.js
@@ -6,7 +6,7 @@ const KEY = process.env.WEATHER_KEY;
 const Service = {
   getWeatherByCity: function (city) {
     return axios
-      .get(`${URL}weather?q=${city}&appid=${KEY}`)
+      .get(`${URL}weather?q=${city}&appid=${KEY}&units=metric`)
       .then((response) => {
         return response.data;
       })
@@ -16,7 +16,7 @@ const Service = {
   },
   getForecastByCity: function (city) {
     return axios
-      .get(`${URL}forecast?q=${city}&appid=${KEY}`)
+      .get(`${URL}forecast?q=${city}&appid=${KEY}&units=metric`)
       .then((response) => {
         return response.data;
       })

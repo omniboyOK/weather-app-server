e9164e7a873ecd77b2915ced7cd7087736940741
omniboyok <barrea.pablo@gmail.com>
2021-04-13 02:31:46 -0300

    Middleware comments

--- src/service/LocationService.js
+++ src/service/LocationService.js
@@ -1,14 +1,20 @@
 const ipapi = require("ipapi.co");
 const Service = {
   getLocation: async function (req, res, next) {
+    /**
+     * Recibimos un parametro city como string
+     * y tomamos el ip de la request
+     */
     const { city } = req.params;
     const { ip } = req;
 
+    // Si tenemos la ciudad se la pasamos a express
     if (city) {
       res.locals.location = req.params.city;
       return next();
     }
 
+    // Si no tenemos la ciudad utilizamos la ip
     if (ip) {
       ipapi.location(function (result) {
         if (result.error) {
@@ -20,6 +26,7 @@ const Service = {
         }
       }, ip);
     } else {
+      // Si no tenemos la ciudad ni acceso a la ip rechazamos la request
       return res
         .status(400)
         .json({ error: "missing header data and city parameter" });
